777da77c3658a1116ff6716415a2d8871c1af204
Rasmus Kaj <kaj@kth.se>
2021-11-17 19:10:29 +0100

    Styling improvements.
    Some css improvements, smaller svg files, added site icon.

--- src/build.rs
+++ src/build.rs
@@ -5,9 +5,22 @@ use syntax_hl::get_css;
 fn main() -> Result<()> {
     let mut ructe = Ructe::from_env()?;
     let mut statics = ructe.statics()?;
+    // Use a standard synect style, but replace some colors for improved contrast.
     statics.add_file_data(
         "shl.css",
-        get_css("Solarized (light)").unwrap().as_ref(),
+        get_css("Solarized (light)")
+            .unwrap()
+            .replace("#268bd2;", "#1578bd;")
+            .replace("#2aa198;", "#115e58;")
+            .replace("#657b83;", "#48585e;")
+            .replace("#6c71c4;", "#575db7;")
+            .replace("#839496;", "#115e58;")
+            .replace("#859900;", "#5b6805;")
+            .replace("#b58900;", "#806100;")
+            .replace("#d33682;", "#bb246d;")
+            .replace("#93a1a1;", "#863135;")
+            .replace("#cb4b16;", "#b73d0b;")
+            .as_ref(),
     )?;
     statics.add_files("res/img")?;
     statics.add_files("res/fonts")?;
