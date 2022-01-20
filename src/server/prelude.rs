c1318f8945a019a941122972fd434b1656b5e37f
Rasmus Kaj <kaj@kth.se>
2022-01-20 15:55:33 +0100

    Rustfmt.

--- src/server/prelude.rs
+++ src/server/prelude.rs
@@ -6,12 +6,14 @@ use reqwest::Url;
 pub fn tweet_url(text: &str, url: &str) -> Url {
     Url::parse_with_params(
         "https://twitter.com/share",
-        &[("text", text), ("url", url), ("via", "rasmus_kaj")]
-    ).unwrap()
+        &[("text", text), ("url", url), ("via", "rasmus_kaj")],
+    )
+    .unwrap()
 }
 pub fn fb_share_url(url: &str) -> Url {
     Url::parse_with_params(
         "https://www.facebook.com/sharer/sharer.php",
-        &[("u", url), ("display", "popup")]
-    ).unwrap()
+        &[("u", url), ("display", "popup")],
+    )
+    .unwrap()
 }
