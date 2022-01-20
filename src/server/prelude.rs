53f16de31d7d41e51f83ed45487ce98a3e2228d7
Rasmus Kaj <kaj@kth.se>
2022-01-20 15:47:38 +0100

    Proper parameter handling for twitter and fb urls.

--- src/server/prelude.rs
+++ src/server/prelude.rs
@@ -1,3 +1,17 @@
 pub use i18n_embed::fluent::FluentLanguageLoader;
 pub use i18n_embed::LanguageLoader;
 pub use i18n_embed_fl::fl;
+use reqwest::Url;
+
+pub fn tweet_url(text: &str, url: &str) -> Url {
+    Url::parse_with_params(
+        "https://twitter.com/share",
+        &[("text", text), ("url", url), ("via", "rasmus_kaj")]
+    ).unwrap()
+}
+pub fn fb_share_url(url: &str) -> Url {
+    Url::parse_with_params(
+        "https://www.facebook.com/sharer/sharer.php",
+        &[("u", url), ("display", "popup")]
+    ).unwrap()
+}
